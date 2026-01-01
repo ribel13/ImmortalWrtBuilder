64da3a006c0f4e5f61238d16079fd6492d192ab3
Atho Belo <181945610+ribel13@users.noreply.github.com>
2026-01-01 13:42:30 +0900

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # Modify default system settings
 
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.100.252/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.1.1' package/base-files/files/bin/config_generate 
 
 # 5G
 echo "src-git 5g https://github.com/Siriling/5G-Modem-Support;main" >> feeds.conf.default
